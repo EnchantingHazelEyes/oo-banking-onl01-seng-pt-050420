5d1d195d7a9fdf0e1f3ae01db2cc3ad76499ddbf
Samantha Barros <samantha.barros95@gmail.com>
2020-06-26 03:57:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,14 +16,15 @@ class Transfer
     end
   end
 
-  def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
+ if @sender.balance < @amount
+       @status = "rejected"
+       return "Transaction rejected. Please check your account balance."
+    elsif @status == "complete"
+      puts "Transaction was already excuted"
     else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
+      @status = "complete"
     end
   end
 
