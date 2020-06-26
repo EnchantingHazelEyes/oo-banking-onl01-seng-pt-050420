2ec15b16b94349e1d3aa6bd3eac05aadb19498d9
Samantha Barros <samantha.barros95@gmail.com>
2020-06-26 04:05:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
 
  if @sender.balance < @amount
        @status = "rejected"
-       return "Transaction rejected. Please check your account balance."
+       puts "Transaction rejected. Please check your account balance."
     elsif @status == "complete"
       puts "Transaction was already excuted"
     else
